73adb388933a36d1c7c8e45ee6d1edbf1741df3b
Noah <cardoson@mcmaster.ca>
2025-01-24 12:42:47 -0500

    [feat] created a MazerSolver that can solve the simplest possible maze, the straight maze

---
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/MazeSolver.java
@@ -0,0 +1,47 @@
+package ca.mcmaster.se2aa4.mazerunner;
+
+import java.util.ArrayList;
+
+public class MazeSolver {
+	private Maze maze;
+	private ArrayList<String> path; //stores the path taken through the maze
+
+	public MazeSolver(Maze maze){
+		this.maze = maze;
+		this.path = new ArrayList<String>();
+	}
+
+	//Only solves the straight maze
+	//Otherwise cannot solve the maze
+	public boolean Solve(){
+		int rows = maze.getRows();
+		int columns = maze.getColumns();
+
+        for(int i = 0; i < rows; i++){
+			//looks for entrance on left side
+			if(maze.get(i, 0) == 0){
+				//keeps moving towards the right untill it hits a wall
+				// or till it reachs the exit
+                for(int j = 0; j < columns; j++){
+					//check if it can move toward the right
+                    if(maze.get(i, j) == 0){
+                        path.add("R");
+                        continue;
+                    }
+					//if it is blocked then no straight path exists
+                    else{
+                        return false;
+                    }
+                }
+				//has reached the end of the maze without hitting a wall
+                return true;
+            }
+		}
+		//no entrance found
+        return false;
+
+	}
+   
+
+
+}
